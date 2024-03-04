3bac51b5adf85194ff8d9e7694b0df1155724de2
nageshxlnet147 <nageshxlnet147@gmail.com>
2024-03-04 13:07:33 +0530

    Tuple sequence content

---
+++ Python/code3.py
@@ -0,0 +1,22 @@
+# Tuple its an hetrogenious (hetro: multiple, genious: types), but we can't change the values once assigned.
+
+tpl1 = (1, 3, 5, 'nagesh', 'roona', 'n', 'r', 1.2, 1.3, 1.4)
+print(tpl1)
+print(type(tpl1))
+
+#Tuple to converstions
+
+#Tuple to string conversion
+string= str(tpl1)
+print(string)
+print(type(string))
+
+#Tuple to set conversion
+set1 = set(tpl1)
+print(set1)
+print(type(set1))
+
+#Tuple to list conversion
+lst1 = list(tpl1)
+print(lst1)
+print(type(lst1))
