b793913dba06fd2f732b511d83d75582c9ed9971
nageshxlnet147 <nageshxlnet147@gmail.com>
2024-03-04 13:09:33 +0530

    Set datatype content

---
+++ Python/code5.py
@@ -0,0 +1,22 @@
+# Set: Each element in the set must be unique.
+
+set1 = {1, 3, 5, 'nagesh', 'roona', 'n', 'r', 1.2, 1.3, 1.4}
+print(set1)
+print(type(set1))
+
+#Set to converstions
+
+#Set to string conversion
+string= str(set1)
+print(string)
+print(type(string))
+
+#Set to set conversion
+dict1 = set(set1)
+print(set1)
+print(type(set1))
+
+#Set to list conversion
+lst1 = list(set1)
+print(lst1)
+print(type(lst1))
