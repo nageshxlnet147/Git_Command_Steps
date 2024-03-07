3017eb4840de4a492c0af74f19012da27efb5972
nageshxlnet147 <nageshxlnet147@gmail.com>
2024-03-07 16:49:11 +0530

    Day4 few notes updated

---
+++ Python/code13.py
@@ -0,0 +1,18 @@
+# Operator precedence & Associativity:
+
+# Unary Operator
+n = -4
+print(+n)
+print(-n)
+
+
+# Binary Operator
+print(2+n)
+print(4-n)
+
+# Ternary Operator
+a = 20
+b = 30
+print(b if a < b else a)
+
+
