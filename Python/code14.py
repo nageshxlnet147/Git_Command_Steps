4d2fc924878f0b435a8bda63f8ea1cdcc36a3e1c
nageshxlnet147 <nageshxlnet147@gmail.com>
2024-03-09 16:26:49 +0530

    Day5 notes and code added

--- Python/code14.py
+++ Python/code14.py
@@ -46,3 +46,44 @@ print(lst2, lst1)
 lst2 = lst1.copy() # here copy() create the shadow of the lst1 and assigned to lst2 so once we update the any list it will update the those list enough not for other list.
 lst1.append("431")
 print(lst1, lst2)
+
+# List remove():
+value_list.remove("python")
+print(value_list)
+
+# List clear():
+lst3 = [1, 2, 3, 'Roona', 5, 1.2]
+lst3.clear()
+print(lst3)
+
+# List insert():
+lst3.insert(0, 'Roona')
+print(lst3)
+
+# List pop():
+lst4 = [1, 2, 3, 5, 1.2]
+lst4.pop()
+print(lst4)
+
+lst4.pop(3) # deleting the particular index item in the list
+print(lst4)
+
+# List reverse():
+lst4.reverse()
+print(lst4)
+
+# List sort():
+lst4.sort()
+print(lst4)
+
+# List sorted():
+print(sorted([1, 2, 3, 5, 1.2]))
+
+# List count():
+print(lst1.count(3))
+
+# List index():
+print(lst1.index(3))
+
+# list slicing[::]:
+print("Positive side:",lst1[2:6], lst1[2:6:2], "Negative side:",lst1[-6:-2], lst1[-6:-2:2])
---
+++ Python/code15.py
@@ -0,0 +1,8 @@
+# Tuple Inbuilt Functions:
+
+# Tuple count()
+lst = [1,4,2,5,6,4]
+print(lst.count(4))
+
+# Tuple index():
+print(lst.index(6))
