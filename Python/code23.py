98dd34c1437809088a9652bd2e420e3be13b334f
nageshxlnet147 <nageshxlnet147@gmail.com>
2024-04-22 13:39:46 +0530

    notes added

--- Python/code23.py
+++ Python/code23.py
@@ -1,11 +1,17 @@
 # OOPS Classes & objcets
+
+# To define a class name as pen.
 class pen():
     refil = 1      # class variable
     colour = 1
-    def __init__(self):     # Objcet initialization
+
+    # To initialize the object data. The self parameter refers to the current instance of the class and its default argument.
+    def __init__(self):     # Object initialization
         self.refil = 1      # Instance variable or object variable
         self.colour = 1
+    
+    # To create the methods like functions or behaviors.
     def write(self):
         print(self.refil)
-book= pen()                 # Object creating by using book variable
-book.write()                # Accessing the class data
+book= pen()                 # Object creating by using book variable.
+book.write()                # Accessing the class data or printing the required data.
